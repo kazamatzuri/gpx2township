acdab21180cae8d1679b03f9b03e9789a02d3928
Fabian Haupt <fhaupt@wayfair.com>
2024-03-08 12:26:08 -0500

    cleanup

--- main.py
+++ main.py
@@ -1,57 +1,75 @@
+import argparse
 from geopy.geocoders import Nominatim
 import gpxpy
 
-VILLAGES=set()
-TOWNSHIPS=set()
-CITIES=set()
-STATES=set()
-geolocator = Nominatim(domain='localhost:8080/', scheme='http',user_agent="myGeocoder")
+VILLAGES = set()
+TOWNSHIPS = set()
+CITIES = set()
+STATES = set()
+geolocator = Nominatim(domain="localhost:8080/", scheme="http", user_agent="myGeocoder")
 
-def get_township(lat, lon):
 
-    # geolocator = Nominatim(domain='localhost:8080/nominatim', scheme='http',user_agent="myGeocoder")    
-    location = geolocator.reverse([lat, lon], exactly_one=True)
-    
+def get_township(latitude, longitude):
+
+    # geolocator = Nominatim(domain='localhost:8080/nominatim', scheme='http',user_agent="myGeocoder")
+    location = geolocator.reverse([latitude, longitude], exactly_one=True)
+
     if location is None:
         return None
-    address = location.raw['address']
-    
-    city = address.get('city', '')
-    village = address.get('village', '')
-    township = address.get('township', '')
-    loc={"city":city,"village":village,"township":township,"state":address.get('state', '')}
-    return loc    
+    address = location.raw["address"]
 
-def parse_gpx_file(file_path):
-    with open(file_path, 'r') as gpx_file:
+    city = address.get("city", "")
+    village = address.get("village", "")
+    township = address.get("township", "")
+    location_info = {
+        "city": city,
+        "village": village,
+        "township": township,
+        "state": address.get("state", ""),
+    }
+    return location_info
+
+
+def parse_gpx_file(gpx_file_path):
+    with open(gpx_file_path, "r") as gpx_file:
         gpx = gpxpy.parse(gpx_file)
 
     for track in gpx.tracks:
         for segment in track.segments:
             for point in segment.points:
-                loc=get_township(point.latitude, point.longitude)
-                if loc is None:
+                location_info = get_township(point.latitude, point.longitude)
+                if location_info is None:
                     continue
-                VILLAGES.add(loc["village"])
-                TOWNSHIPS.add(loc["township"])
-                CITIES.add(loc["city"])
-                STATES.add(loc["state"])
+                VILLAGES.add(location_info["village"])
+                TOWNSHIPS.add(location_info["township"])
+                CITIES.add(location_info["city"])
+                STATES.add(location_info["state"])
                 # print('Lat/Lon:', point.latitude, point.longitude)
 
-# Example usage:
-file="TAT FAB 2023.gpx"
-parse_gpx_file(file)
 
-print("STATES")
-for s in STATES:
-    print(s)
-# print("VILLAGES")
-# for v in VILLAGES:
-#     print(v)
-# print("TOWNSHIPS")
-# for t in TOWNSHIPS:
-#     print(t)
-# print("CITIES")
-# for c in CITIES:
-#     print(c)
-# print(get_township(43.4805667, -071.7565000))  # Coordinates for New York City
+def main():
+    parser = argparse.ArgumentParser(
+        description="Parse a GPX file and extract township information."
+    )
+    parser.add_argument("gpx_file_path", help="The path to the GPX file to parse.")
+    args = parser.parse_args()
+
+    parse_gpx_file(args.gpx_file_path)
+
+    print("STATES")
+    for s in STATES:
+        print(s)
+    print("VILLAGES")
+    for v in VILLAGES:
+        print(v)
+    print("TOWNSHIPS")
+    for t in TOWNSHIPS:
+        print(t)
+    # print("CITIES")
+    # for c in CITIES:
+    #     print(c)
+    # print(get_township(43.4805667, -071.7565000))  # Coordinates for New York City
+
+
+if __name__ == "__main__":
+    main()
--- readme.md
+++ readme.md
@@ -1,2 +1,19 @@
+# deps
 
+```
+poetry install
+```
+
+run:
+
+```
 docker run -it -e PBF_URL=http://download.geofabrik.de/north-america/us-northeast-latest.osm.pbf -e shm-size=8GB -p 8080:8080 --name nominatim mediagis/nominatim:4.2
+```
+
+(takes a while to initialize)
+
+then
+
+```
+python main.py name-to-gpx.gpx
+```
